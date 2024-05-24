34ee95ddcb76f71062398e3ca51f5bbdcf3bf17b
Horaciodiaz <horacio.diaz.2000@gmail.com>
2024-05-24 20:25:42 -0300

    para entregar

--- script.js
+++ script.js
@@ -47,3 +47,98 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     obtenerProductos();
 });
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    function validateForm(event) {
+        event.preventDefault();
+        
+        const form = document.querySelector('#contactoForm');
+            const inputs = form.querySelectorAll('.control');
+        let isValid = true;
+
+        var mensajeDiv = document.querySelector('#requisitos')
+        setTimeout(function() {
+            mensajeDiv.style.display = 'none';
+        }, 7000);
+        mensajeDiv.innerHTML = '';
+        mensajeDiv.style.display = 'none';
+        
+        var emailInput = document.querySelector('#email').value;
+
+        if (emailInput.trim() !== '') {
+            
+            if (emailInput.endsWith('@gmail.com')) {
+                console.log('El correo electrónico termina en @gmail.com.');
+            } else {
+                var parrafo = document.createElement('p');
+    
+                parrafo.textContent = '• El correo electrónico no termina en @gmail.com.';
+                mensajeDiv.appendChild(parrafo);
+    
+                mensajeDiv.style.display = 'block';
+    
+                formularioValido = false;
+
+            }
+        }
+
+        var passwordInput = document.querySelector('#password').value;
+        var repasswordInput = document.querySelector('#repassword').value;
+        
+        if (passwordInput.trim() !== '') {
+            if (passwordInput.length < 8 || passwordInput.length > 16) {
+                var parrafo = document.createElement('p');
+    
+                parrafo.textContent = '• La contraseña debe tener entre 8 y 16 caracteres.';
+                mensajeDiv.appendChild(parrafo);
+    
+                mensajeDiv.style.display = 'block';
+    
+                formularioValido = false;
+            }
+    
+            if (passwordInput !== repasswordInput) {
+                var parrafo = document.createElement('p');
+    
+                parrafo.textContent = '• Las contraseñas deben coincidir';
+                mensajeDiv.appendChild(parrafo);
+    
+                mensajeDiv.style.display = 'block';
+    
+                formularioValido = false;
+            }
+        } 
+
+        for (var i = 0; i < inputs.length; i++) {
+            if (inputs[i].value.trim() === '') {
+                var parrafo = document.createElement('p');
+                
+                var msjerror = inputs[i].placeholder;
+                
+                parrafo.textContent = '• ' + msjerror;
+                mensajeDiv.appendChild(parrafo);
+    
+                // Mostrar el div de mensaje
+                mensajeDiv.style.display = 'block';
+    
+                // Cambiar el estado del formulario a no válido
+                formularioValido = false;
+                
+            }
+        }
+
+        if (isValid) {
+            alert('Formulario válido!');
+            
+        }
+        
+    }   
+
+    const submitButton = document.querySelector('#submitButton');
+    submitButton.addEventListener('click', validateForm);
+});
+
+
+
